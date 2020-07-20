9c18fb6fe0d977da835afef700a318a564188ac5
Tycho <tycho.bellers999@gmail.com>
2020-07-20 12:31:05 -0700

    UI improvements.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ class NeuralNetwork extends HTMLElement{
 
     constructor(){
         super();
-        this._layer_sizes = [2, 3, 2];
-        this._layer_colors = ['red', 'blue', 'green'];
+        this._layer_sizes = [2, 3, 3, 2];
+        this._layer_colors = ['red', 'blue', 'blue', 'green'];
     }
 
     connectedCallback(){
@@ -17,8 +17,7 @@ class NeuralNetwork extends HTMLElement{
     _applyStyle(){
         this.style.display = 'flex';
         this.style.justifyContent = 'center';
-        this.style.gap = '100px';
-        //this.style.backgroundColor = 'black';
+        this.style.gap = '20px';
 
         for (let i = 0; i < this._layer_sizes.length; i++){
             const layer_div = document.createElement('div');
@@ -26,22 +25,28 @@ class NeuralNetwork extends HTMLElement{
             layer_div.style.display = 'flex';
             layer_div.style.flexDirection = 'column';
             layer_div.style.padding = '8px';
-            //layer_div.style.backgroundColor = 'purple';
 
             //Layer name
             const layer_name_label = document.createElement('label');
-            layer_name_label.innerHTML = `Layer ${i}`;
+            if (i == 0){
+                layer_name_label.innerHTML = `Input`;
+            }else if (i == this._layer_sizes.length - 1){
+                layer_name_label.innerHTML = `Output`;
+            }else{
+                layer_name_label.innerHTML = `Layer ${i}`;
+            }
             layer_name_label.style.paddingBottom = '16px';
+            layer_name_label.style.textAlign = 'center';
             layer_div.appendChild(layer_name_label);
 
             //Neurons
             const neurons_div = document.createElement('div');
             neurons_div.style.flexGrow = '1';
-            //neurons_div.style.backgroundColor = 'coral';
             neurons_div.style.display = 'flex';
             neurons_div.style.flexDirection = 'column';
             neurons_div.style.alignItems = 'center';
             neurons_div.style.justifyContent = 'center';
+            neurons_div.style.gap = '20px';
             for (let j = 0; j < this._layer_sizes[i]; j++){
                 const neuron = document.createElement('svg');
                 neuron.setAttribute('id', `${layer_div.id}_neuron_${j}`);
@@ -53,6 +58,9 @@ class NeuralNetwork extends HTMLElement{
 
             this.appendChild(layer_div);
         }
+
+        const PREF_GAP = this.getBoundingClientRect().width / (this._layer_sizes.length - 1) - 100;
+        this.style.gap = `${Math.min(PREF_GAP, 300)}px`;
 
         //Create connections
         for (let i = 1; i < this._layer_sizes.length; i++){
@@ -74,27 +82,90 @@ class NeuralNetwork extends HTMLElement{
                     const distance = Math.sqrt(Math.pow(dst_x - src_x, 2) + Math.pow(dst_y - src_y, 2));
                     const angle = Math.atan2(dst_y - src_y, dst_x - src_x);
 
-                    const center_x = (src_x + dst_x - distance) / 2;
-                    const center_y = (src_y + dst_y - 4) / 2; //-4 for line thickness
+                    const center_x = (src_x + dst_x) / 2;
+                    const center_y = (src_y + dst_y) / 2;
+
+                    const connection_label = document.createElement('label');
+                    connection_label.setAttribute('id', `weight_label_${src_neuron.id}_${dst_neuron.id}`);
+                    connection_label.innerHTML = '0.35';
+                    connection_label.style.padding = '4px';
+                    connection_label.style.borderRadius = '4px';
+                    connection_label.style.zIndex = 2;
+                    connection_label.style.backgroundColor = 'black'
+                    connection_label.style.position = 'absolute';
+                    dst_layer_div.appendChild(connection_label);
+                    connection_label.style.left = `${center_x - (connection_label.getBoundingClientRect().width / 2)}px`;
+                    connection_label.style.top = `${center_y - (connection_label.getBoundingClientRect().height / 2) - (4 / 2)}px`; //-4 /2 for baseline offset
+                    connection_label.style.transform = `rotate(${angle}rad)`;
+                    connection_label.hidden = true;
 
                     const connection = document.createElement('div');
                     connection.style.position = 'absolute';
                     connection.style.width = `${distance}px`;
                     connection.style.height = '1px';
                     connection.style.backgroundColor = 'gray';
-                    connection.style.top = `${center_y}px`
-                    connection.style.left = `${center_x}px`
-                    connection.style.transform = `rotate(${angle}rad)`
+                    connection.style.top = `${center_y - (4 / 2)}px`; //-4 for line thickness
+                    connection.style.left = `${center_x - (distance / 2)}px`;
+                    connection.style.transform = `rotate(${angle}rad)`;
                     connection.addEventListener('mouseenter', (event) => {
                         connection.style.backgroundColor = 'white';
                         connection.style.zIndex = 1;
+                        connection_label.hidden = false;
                     })
                     connection.addEventListener('mouseleave', (event) => {
                         connection.style.backgroundColor = 'gray';
                         connection.style.zIndex = 0;
+                        connection_label.hidden = true;
                     })
                     dst_layer_div.appendChild(connection);
                 }
+            }
+        }
+
+        const getSourceNeurons = (layer_index) => {
+            layer_index--;
+            const neurons = [];
+            if (layer_index >= 0){
+                for (let i = 0; i < this._layer_sizes[layer_index]; i++){
+                    neurons.push(document.getElementById(`layer_${layer_index}_neuron_${i}`));
+                }
+            }
+            return neurons;
+        }
+
+        function getDestinationNeurons(layer_index){
+            return getSourceNeurons(layer_index + 2);
+        }
+
+        //Apply mouse over for neurons
+        for (let i = 0; i < this._layer_sizes.length; i++){
+            const layer_div = document.getElementById(`layer_${i}`);
+            for (let j = 0; j < this._layer_sizes[i]; j++){
+                const neuron = document.getElementById(`${layer_div.id}_neuron_${j}`);
+                neuron.addEventListener('mouseenter', (event) => {
+                    const src_neurons = getSourceNeurons(i);
+                    for (let k = 0; k < src_neurons.length; k++){
+                        const connection_label = document.getElementById(`weight_label_${src_neurons[k].id}_${neuron.id}`);
+                        connection_label.hidden = false;
+                    }
+                    const dst_neurons = getDestinationNeurons(i);
+                    for (let k = 0; k < dst_neurons.length; k++){
+                        const connection_label = document.getElementById(`weight_label_${neuron.id}_${dst_neurons[k].id}`);
+                        connection_label.hidden = false;
+                    }
+                });
+                neuron.addEventListener('mouseleave', (event) => {
+                    const src_neurons = getSourceNeurons(i);
+                    for (let k = 0; k < src_neurons.length; k++){
+                        const connection_label = document.getElementById(`weight_label_${src_neurons[k].id}_${neuron.id}`);
+                        connection_label.hidden = true;
+                    }
+                    const dst_neurons = getDestinationNeurons(i);
+                    for (let k = 0; k < dst_neurons.length; k++){
+                        const connection_label = document.getElementById(`weight_label_${neuron.id}_${dst_neurons[k].id}`);
+                        connection_label.hidden = true;
+                    }
+                });
             }
         }
     }
@@ -103,38 +174,3 @@ class NeuralNetwork extends HTMLElement{
 customElements.define('neural-network', NeuralNetwork);
 let e = document.createElement('neural-network');
 document.body.appendChild(e);
-
-
-
-/* function getOffset( el ) {
-    var rect = el.getBoundingClientRect();
-    return {
-        left: rect.left + window.pageXOffset,
-        top: rect.top + window.pageYOffset,
-        width: rect.width || el.offsetWidth,
-        height: rect.height || el.offsetHeight
-    };
-}
-
-function connect(div1, div2, color, thickness) { // draw a line connecting elements
-    var off1 = getOffset(div1);
-    var off2 = getOffset(div2);
-    // bottom right
-    var x1 = off1.left + off1.width;
-    var y1 = off1.top + off1.height;
-    // top right
-    var x2 = off2.left + off2.width;
-    var y2 = off2.top;
-    // distance
-    var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
-    // center
-    var cx = ((x1 + x2) / 2) - (length / 2);
-    var cy = ((y1 + y2) / 2) - (thickness / 2);
-    // angle
-    var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
-    // make hr
-    var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
-    //
-    // alert(htmlLine);
-    document.body.innerHTML += htmlLine;
-} */
