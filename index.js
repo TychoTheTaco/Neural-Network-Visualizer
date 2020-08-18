046c35edfaeaafb3237b6aaee641cb71171fec9d
Tycho Bellers <tycho.bellers999@gmail.com>
2020-08-18 16:38:09 -0700

    Started adding interactive canvas

--- index.js
+++ index.js
@@ -552,7 +552,7 @@ class NeuralNetworkElement extends HTMLElement{
                     connection.style.width = `${distance}px`;
                     connection.style.height = '1px';
                     connection.style.backgroundColor = 'gray';
-                    connection.style.top = `${center_y - (1 / 2)}px`; //-1 for line thickness
+                    connection.style.top = `${center_y - (2 / 2)}px`; //-2 (/2) for line thickness
                     connection.style.left = `${center_x - (distance / 2)}px`;
                     connection.style.transform = `rotate(${angle}rad)`;
                     /* connection.addEventListener('mouseenter', (event) => {
@@ -838,7 +838,7 @@ single_step_button.addEventListener('click', (event) => {
         neural_network_element._deselectAll();
         neural_network_element.selectNeuronAndWeights(i, j, [neural_network_element._neural_network._weight_index]);
         current_step_label.innerHTML = 'Backpropagation';
-        neural_network_element._calculation_details_div.innerHTML = 'Backpropagation is the process of adjusting the weights and biases of each neuron in order to lower the total error (cost). To do this, we need to calculate the gradient of the cost function with respect to the output of each neuron in the last layer.';
+        neural_network_element._calculation_details_div.innerHTML = 'Backpropagation is the process of adjusting the weights and biases of each neuron in an attempt to lower the total error, or cost, of our neural network. To do this, we need to calculate the gradient of the cost function with respect to the output of each neuron in the last layer.';
 
         box.style.display = 'none';
 
@@ -961,3 +961,62 @@ random_configuration_button.addEventListener('click', (event) => {
 
     neural_network_element.setNeuralNetwork(neural_network);
 });
+
+
+class DrawCanvas{
+
+    constructor(canvas){
+        this._canvas = canvas;
+
+        this._ctx = input_canvas.getContext('2d');
+        this._ctx.lineWidth = 10;
+        this._ctx.lineCap = 'round';
+        this._ctx.strokeStyle = 'white';
+
+        this._isMouseDown = false;
+        this._last_x = 0;
+        this._last_y = 0;
+
+        canvas.addEventListener('mousemove', (event) => {
+            if (this._isMouseDown){
+                this._ctx.beginPath();
+                this._ctx.moveTo(this._last_x, this._last_y);
+                const bounds = event.target.getBoundingClientRect();
+                this._last_x = event.clientX - bounds.left;
+                this._last_y = event.clientY - bounds.top;
+                this._ctx.lineTo(this._last_x, this._last_y);
+                this._ctx.stroke();
+            }
+        });
+
+        canvas.addEventListener('mousedown', (event) => {
+            this._isMouseDown = true;
+
+            const bounds = event.target.getBoundingClientRect();
+            this._last_x = event.clientX - bounds.left;
+            this._last_y = event.clientY - bounds.top;
+            this._ctx.moveTo(this._last_x, this._last_y);
+        })
+
+        canvas.addEventListener('mouseup', (event) => {
+            this._isMouseDown = false;
+
+            //const image_data = this._ctx.getImageData(0, 0, this._canvas.width, this._canvas.height);
+        })
+
+        canvas.addEventListener('mouseleave', (event) => {
+            this._isMouseDown = false;
+        })
+
+        this._clear_canvas_button = document.getElementById('clear_canvas_button');
+        this._clear_canvas_button.addEventListener('click', (event) => {
+            this.clear();
+        })
+    }
+
+    clear(){
+        this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    }
+}
+
+//const drawCanvas = new DrawCanvas(document.getElementById('input_canvas'));
